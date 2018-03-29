7bea9ff5f323c7b1c57877d49b6bd735a4876568
serhii <total040@gmail.com>
2018-03-29 21:49:09 +0200

    add get Todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require ('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {User} = require('./models/users');
@@ -27,6 +28,24 @@ app.get('/Todos', (req, res) => {
         res.send({todos});
     }, (err) => {
         res.status(400).send(err);
+    });
+
+});
+
+app.get('/Todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send();
+        return;
+    }
+    Todo.findById(id).then((todo) => {
+        if (todo) {
+            res.send({todo});
+        } else {
+            res.status(404).send();
+        }
+    }, (err) => {
+        res.status(400).send();
     });
 
 });
