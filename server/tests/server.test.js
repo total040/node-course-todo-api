2cbf121370826a890f1fa7e569478a98ea0d6d58
serhii <total040@gmail.com>
2018-03-29 22:25:02 +0200

    add get Todos/:id test suite

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todos');
 
 const todos = [{
+    _id: new ObjectID(),
     text: 'todo number 1'
 }, {
+    _id: new ObjectID(),
     text: 'todo number 2'
 }];
 
@@ -69,5 +72,35 @@ describe('GET /Todos', () => {
                expect(result.body.todos.length).toBe(2);
             })
             .end(done);
+    });
+});
+
+describe('GET Todos/:id', () => {
+   it('should return valid todo', (done) => {;
+       request(app)
+           .get(`/Todos/${todos[0]._id.toHexString()}`)
+           .expect(200)
+           .expect((res) => {
+               expect(res.body.todo.text).toBe(todos[0].text);
+           })
+           .end(done);
+
+   });
+
+    it('should return 404 for todo with bad id', (done) => {
+        request(app)
+            .get(`/Todos/343`)
+            .expect(404)
+            .end(done);
+
+    });
+
+    it('should return 404 for todo not found', (done) => {
+        var badId = new ObjectID().toHexString();
+        request(app)
+            .get(`/Todos/${badId}`)
+            .expect(404)
+            .end(done);
+
     });
 });
